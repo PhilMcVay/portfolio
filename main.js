f257b520332dff15d6621c372c34374d651bc4c5
Phil McVay <phil.mcvay90@gmail.com>
2018-11-18 14:57:14 +0000

    Initial Commit

---
+++ main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+  // Add background to nav when scrolled past top of the page
+  // Toggle scroll to top button
+  addNavBackgound()
+  renderScrollTopBtn()
+  $(window).scroll(function() {
+    addNavBackgound()
+    renderScrollTopBtn()
+  })
+
+  // Close mobile menu on window expansion
+  $(window).resize(function() {
+    if (window.innerWidth > 630) {
+      closeMobileMenu()
+    }
+  })
+
+  // Scroll animation on nav link click
+  $('nav a').click(function() {
+    let section = $(this).attr('href')
+    let navHeight = $('.nav-wrapper').height()
+    $("html, body").animate({
+      scrollTop: $(section).offset().top - navHeight
+    }, 1000, "easeInOutExpo");
+  })
+
+  // Toggle mobile menu
+  $('.hamburger').click(function() {
+    $(this).toggleClass('is-active')
+    $('.mobile-menu').toggleClass('open')
+  })
+
+  // Close mobile menu on nav link click
+  $('.mobile-menu a').click(function() {
+    closeMobileMenu()
+  })
+
+  // Scroll to top of page on click
+  $('.scroll-top-btn').click(function() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000, "easeInOutExpo");
+  })
+
+});
+
+function addNavBackgound() {
+  let scrollPosition = $(window).scrollTop()
+    scrollPosition > 0
+      ? $('.nav-wrapper').addClass('scrolled-down')
+      : $('.nav-wrapper').removeClass('scrolled-down')
+}
+
+function renderScrollTopBtn() {
+  let scrollPosition = $(window).scrollTop()
+    scrollPosition > 500
+      ? $('.scroll-top-btn').addClass('scroll-top-active')
+      : $('.scroll-top-btn').removeClass('scroll-top-active')
+}
+
+function closeMobileMenu() {
+  $('.hamburger').removeClass('is-active')
+  $('.mobile-menu').removeClass('open')
+}
